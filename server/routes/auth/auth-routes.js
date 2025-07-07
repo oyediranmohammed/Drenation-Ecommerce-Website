2466f8fc328daa6cb6dbf134b8e91fa27cd03c06
Mohammed <95849758+oyediranmohammed@users.noreply.github.com>
2025-07-08 01:43:42 +0400

    Completed Authentication and protected routes

--- server/routes/auth/auth-routes.js
+++ server/routes/auth/auth-routes.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import registerUser from '../../controllers/auth/auth-controller.js';
+import {registerUser, loginUser, logoutUser, authMiddleware} from '../../controllers/auth/auth-controller.js';
 
-const authRouter = express.Router();
+const router = express.Router();
 
-authRouter.post('/register', registerUser);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
+router.get('/check-auth', authMiddleware, (req, res)=> {
+    const user = req.user;
+    res.status(200).json({
+        success : true,
+        message : "Authenticated User",
+        user,
+    });
+});
 
-export default authRouter;
+export default router;
